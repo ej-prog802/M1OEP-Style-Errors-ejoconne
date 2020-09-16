ac781dc1d772429b636da04ec6cb936d24695f9f
ej-prog802 <Emmett.OConnell@uvm.edu>
2020-09-15 20:47:07 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/StyleAndError.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StyleAndError.dir/main.cpp.o"
+  "StyleAndError"
+  "StyleAndError.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StyleAndError.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
